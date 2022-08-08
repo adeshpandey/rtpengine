01dc430ecc8c821a1e2deebfd63d41f2423baf68
adesh <adesh.pandey10@gmail.com>
2022-08-08 23:15:19 +0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rtpengine
 Simple RTPEngine docker-image
+
+#### How to launch?
+    1. mv env.example .env
+    2. set the values
+    3. ``` docker-compose up ```
+
---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+MY_IP=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+sed -i -e "s/MY_IP/$MY_IP/g" /etc/rtpengine.conf
+sed -i -e "s/PUBLIC_IP/$PUBLIC_IP/g" /etc/rtpengine.conf
+sed -i -e "s/MIN_PORT/$MIN_PORT/g" /etc/rtpengine.conf
+sed -i -e "s/MAX_PORT/$MAX_PORT/g" /etc/rtpengine.conf
+
+rtpengine --config-file /etc/rtpengine.conf  "$@"
